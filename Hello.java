b60aa704deb2fc64d3d517f34fd89c5a7e2d10f9
bhsangwan <bhsangwan@gmail.com>
2021-07-22 16:14:03 +0530

    Commited modification without staging

--- Hello.java
+++ Hello.java
@@ -2,10 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello World..!");
         System.out.println("Bye Bye");
-        s  ks kdaj sfd d
-         d 
-         d ;d kdfl 
-         d nlkdl d
-         
+        System.out.println("Hi");
     }
 }
